d06a3d67b14fd66a235a87511341ddfd2fb0e54e
Justin McIntyre <justinmcintyre42@gmail.com>
2022-06-29 15:26:00 -0300

    debugging trades

--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -12,6 +12,8 @@ contract Exchange {
 	address constant ETHER = address(0); //save space by storing Eth in the tokens mapping
 	mapping(uint256=> _Order)public orders;
 	uint256 public orderCount;
+	mapping(uint256=>bool) public ordersCancelled;
+	mapping(uint256=>bool) public ordersFilled;
 
 	event Deposit(address token, address user, uint256 amount, uint256 balance);
 	event Withdrawal(address token, address user, uint256 amount, uint256 balance);
@@ -22,6 +24,29 @@ contract Exchange {
 		uint amountGet,
 		address tokenGive,
 		uint amountGive,
+		uint timestamp
+	);
+	
+	event Cancel(
+		uint id,
+		address user,
+		address tokenGet,
+		uint amountGet,
+		address tokenGive,
+		uint amountGive,
+		uint timestamp
+	);
+
+		event Trade(
+		uint id,
+		address user,
+		address tokenGet,
+		uint amountGet,
+		address tokenGive,
+		uint amountGive,
+		address filledBy,
+		address feeToken,
+		uint feeAmount,
 		uint timestamp
 	);
 
@@ -73,5 +98,47 @@ contract Exchange {
 		orderCount = orderCount.add(1);
 		orders[orderCount] = _Order(orderCount, msg.sender, _tokenGet, _amountGet, _tokenGive, _amountGive, now);
 		emit Order(orderCount, msg.sender, _tokenGet, _amountGet, _tokenGive, _amountGive, now);
+	}
+
+	function _fetchOrder(uint256 _id) internal returns(_Order storage) {
+		_Order storage _order = orders[_id];
+		require(_order.id == _id);
+		return _order;
+	}
+
+	function cancelOrder(uint256 _id) public {
+		_Order storage _order = _fetchOrder(_id);
+		//must be user's order
+		require(address(_order.user) == msg.sender);
+		ordersCancelled[_id] = true;
+		emit Cancel(_order.id, msg.sender, _order.tokenGet, _order.amountGet, _order.tokenGive, _order.amountGive, now);
+	}
+
+	function fillOrder(uint256 _id) public {
+		require(!ordersFilled[_id]);
+		require(!ordersCancelled[_id]);
+		//fetch the order
+		_Order storage _order = _fetchOrder(_id);
+		_trade(_order.id, _order.user, _order.tokenGet, _order.amountGet, _order.tokenGive, _order.amountGive);
+		//mark order as filled
+		ordersFilled[_order.id] = true;
+	}
+
+	function _trade(uint256 _orderId, address _user, address _tokenGet, uint256 _amountGet, address _tokenGive, uint256 _amountGive) internal {
+		
+		uint256 _feeAmount = _amountGet.mul(feePercent).div(100);
+		//execute the order
+		tokens[_tokenGet][msg.sender] = tokens[_tokenGet][msg.sender].sub(_amountGet.add(_feeAmount));
+		tokens[_tokenGet][_user] = tokens[_tokenGet][_user].add(_amountGet);
+		//charge fees
+		tokens[_tokenGet][feeAccount] = tokens[_tokenGet][feeAccount].add(_feeAmount);
+		tokens[_tokenGive][_user] = tokens[_tokenGive][_user].sub(_amountGive);
+		tokens[_tokenGive][msg.sender] = tokens[_tokenGive][msg.sender].add(_amountGive);
+		//emit trade event
+		emit Trade(_orderId, _user, _tokenGet, _amountGet, _tokenGive, _amountGive, msg.sender, _tokenGet, _feeAmount, now);
+	}
+
+	function balanceOf(address _token, address _user) public view returns (uint256) {
+			return tokens[_token][_user];
 	}
 }
