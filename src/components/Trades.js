9d739b02a2ebcb03c9eba4c33d75312dca785955
Justin McIntyre <justinmcintyre42@gmail.com>
2022-08-03 23:59:23 -0300

    set up trades window

--- src/components/Trades.js
+++ src/components/Trades.js
@@ -1,16 +1,49 @@
 import React from "react";
 import { connect } from "react-redux";
+import { filledOrdersSelector, filledOrdersLoadedSelector } from "../store/selectors";
+import Spinner from "./Spinner";
 
 const Trades = (props) => {
+
+	const buildOrdersTableRows = () => {
+		return props.filledOrders.map((order) => {
+			return (
+				<tr className={`order-${order.id}`} key={order.id}>
+					<td className="text-muted">{ order.formattedTimestamp }</td>
+					<td>{ order.tokenAmount }</td>
+					<td className={`text-${order.tokenPriceClass}`} >{ order.tokenPrice }</td>
+				</tr>
+			)
+		})
+	}
+
+	const ordersTableRows = buildOrdersTableRows()
+
 	return (
 		<div className="vertical">
 			<div className="card bg-dark text-white">
 				<div className="card-header">
-					Card Title
+					Trades
 				</div>
 				<div className="card-body">
-					<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-					<a href="/#" className="card-link">Card link</a>
+					<table className="table table-dark table-sm small">
+						<thead><tr>
+							<th>Time</th>
+							<th>SHINO</th>
+							<th>SHINO/ETH</th>
+						</tr></thead>
+						<tbody>
+							{
+							(()=>{
+								if(ordersTableRows.length){
+									return ordersTableRows;
+								} else {
+									return <Spinner type='table' />
+								}
+							})()
+							}
+						</tbody>
+					</table>
 				</div>
 			</div>
 		</div>
@@ -19,7 +52,8 @@ const Trades = (props) => {
 
 function mapStateToProps(state) {
 	return {
-
+		filledOrdersLoaded: filledOrdersLoadedSelector(state),
+		filledOrders: filledOrdersSelector(state)
 	}
 }
 
--- src/store/selectors.js
+++ src/store/selectors.js
@@ -1,5 +1,7 @@
 import { get } from 'lodash'
 import { createSelector } from 'reselect'
+import { ETHER_ADDRESS, tokens, ether, GREEN, RED } from '../helpers'
+import moment from 'moment'
 
 const account = state => get(state, 'web3.account')
 export const accountSelector = createSelector(account, a => a)
@@ -19,3 +21,77 @@ export const contractsLoadedSelector = createSelector(
 	exchangeLoaded,
 	(tl, el)=>(tl && el)
 )
+
+const filledOrdersLoaded = state => get(state, 'exchange.filledOrders.loaded', false)
+export const filledOrdersLoadedSelector = createSelector(filledOrdersLoaded, el => el)
+
+const filledOrders = state => get(state, 'exchange.filledOrders.data', [])
+export const filledOrdersSelector = createSelector(
+	filledOrders,
+	(orders) => {
+		//sort acending timestamp for decoration
+		orders = orders.sort((a,b) => a.timestamp - b.timestamp)
+		
+		// Decorate order
+		orders = decorateFilledOrders(orders)
+
+		// Return desending timestamp
+		orders = orders.reverse()
+
+		return orders
+	}
+)
+
+const decorateFilledOrders = (orders) => {
+	let previousOrder = orders[0]
+	return orders.map((order) => {
+		order = decorateOrder(order)
+		order = decorateFilledOrder(order, previousOrder)
+		previousOrder = order
+		return order
+	})
+}
+
+const decorateOrder = (order) => {
+	let etherAmount
+	let tokenAmount
+	if(order.tokenGive === ETHER_ADDRESS){
+		etherAmount = order.amountGive
+		tokenAmount = order.amountGet
+	} else {
+		etherAmount = order.amountGive
+		tokenAmount = order.amountGet
+	}
+
+	let tokenPrice = (etherAmount / tokenAmount)
+	const precision = 100000
+	tokenPrice = Math.round(tokenPrice * precision) / precision
+
+	return {
+		...order,
+		etherAmount: ether(etherAmount),
+		tokenAmount: tokens(tokenAmount),
+		tokenPrice,
+		formattedTimestamp: moment.unix(order.timestamp).format('h:mm:ss a M/D')
+	}
+}
+
+const decorateFilledOrder = (order, previousOrder) => {
+	return {
+		...order,
+		tokenPriceClass: tokenPriceClass(order.tokenPrice, order.id, previousOrder)
+	}
+}
+
+const tokenPriceClass = (tokenPrice, orderId, previousOrder) => {
+	if(previousOrder.id === orderId) {
+		return GREEN
+	}
+	// Show green price if order pricer higher than previous order
+	// Show red price if order price lower than previous order
+	if(previousOrder.tokenPrice <= tokenPrice) {
+		return GREEN // success boostrap class
+	} else {
+		return RED // danger boostrap class
+	}
+}
